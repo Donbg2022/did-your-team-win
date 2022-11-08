a51ec81e9f46b4ea73da939574e0feb32d882eb2
Donovan <donovangriffin04@gmail.com>
2022-11-08 12:02:07 -0800

    created a new function instead of having avery long promise chain

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
+//api fetch to get game boxscore
+const gameIdLocater = axios.get('https://statsapi.web.nhl.com/api/v1/teams/23?expand=team.schedule.previous')
+.then((res) =>{
+  //getting the game id and using a template literal to insert into a new api get request which will get the last game played 
+  let id = res.data.teams[0].previousGameSchedule.dates[0].games[0].gamePk
+  let nhlGameId = `https://statsapi.web.nhl.com/api/v1/game/${id}/boxscore`;
+  const nhlStats = axios.get(nhlGameId)
+  .then(whoWon)
+  })
+  let nucksGoals = 0
+  let other = 0
+
+
+  //function to determine the winner of most previous game
+function whoWon(result){
+  if (result.data.teams.home.team.name === 'Vancouver Canucks'){
+    //canucks and other team goal count updated
+let nucksGoals = result.data.teams.home.teamStats.teamSkaterStats.goals
+let other = result.data.teams.away.teamStats.teamSkaterStats.goals
+
+//return an if statement 
+if (nucksGoals > other){
+ return 'winnner' + nucksGoals
+}else if (nucksGoals === other){
+  return 'OT' + nucksGoals + '-' + other
+}else {
+  return loser
+}
+
+}else if (result.data.teams.home.team.name !== 'Vancouver Canucks'){
+let nucksGoals = result.data.teams.away.teamStats.teamSkaterStats.goals
+let other = result.data.teams.home.teamStats.teamSkaterStats.goals
+
+if (nucksGoals > other){
+  return 'winnner' + nucksGoals
+}else if (nucksGoals === other){
+  return 'OT' + nucksGoals + '-' + other
+}else {
+  return loser
+}
+}
+}
+
+
+
+const roster = axios.get('https://statsapi.web.nhl.com/api/v1/teams/23/roster')
+
 
 //adding reacctivity to the navbar
 const burger = document.querySelector('#burger')
@@ -19,58 +66,4 @@ burger.addEventListener('click', function(){
 const img = document.querySelector('img')
 const winner = document.querySelector('#winbtn')
 
-// winner.addEventListener('click', winOrLose)
-
-// function winOrLose(){
-  
-// }
-
-
-//api fetch to get game boxscore
-const gameIdLocater = axios.get('https://statsapi.web.nhl.com/api/v1/teams/23?expand=team.schedule.previous')
-.then((res, goals) =>{
-  //getting the game id and using a template literal to insert into a new api get request which will get the last game played 
-  let id = res.data.teams[0].previousGameSchedule.dates[0].games[0].gamePk
-  let nhlGameId = `https://statsapi.web.nhl.com/api/v1/game/${id}/boxscore`;
-  const nhlStats = axios.get(nhlGameId)
-  .then((result) => {
-    
-     if (result.data.teams.home.team.name === 'Vancouver Canucks'){
-      //canucks and other team goal count updated
-  let nucksGoals = result.data.teams.home.teamStats.teamSkaterStats.goals
-  let other = result.data.teams.away.teamStats.teamSkaterStats.goals
-
-  //return an if statement 
-  if (nucksGoals > other){
-    console.log('winnner', nucksGoals)
-  }else if (nucksGoals === other){
-    console.log('OT', nucksGoals, '-', other)
-  }else {
-    console.log('loser')
-  }
-
-  }else if (result.data.teams.home.team.name !== 'Vancouver Canucks'){
-  let nucksGoals = result.data.teams.away.teamStats.teamSkaterStats.goals
-  let other = result.data.teams.home.teamStats.teamSkaterStats.goals
-
-  if (nucksGoals > other){
-    console.log('winnner', nucksGoals)
-  }else if (nucksGoals === other){
-    console.log('OT', nucksGoals, '-', other)
-  }else {
-    console.log('loser')
-  }
-}
-  })
-  let nucksGoals = 0
-  let other = 0
-
-console.log(nucksGoals)
-  console.log(nhlStats)
-})
-
-
-
-
-const roster = axios.get('https://statsapi.web.nhl.com/api/v1/teams/23/roster')
-
+winner.addEventListener('click', gameIdLocater);
