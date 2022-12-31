c454148701331eec9f78f884746c91ae48cb3000
Donovan <donovangriffin04@gmail.com>
2022-12-30 20:10:51 -0800

    fixed error where next game button diplayed the wrong day

--- modules/schedule.js
+++ modules/schedule.js
@@ -6,8 +6,9 @@ async function statType(){
 
   const homeTeam = data.data.teams[0].nextGameSchedule.dates[0].games[0].teams.home.team.name
   const awayTeam = data.data.teams[0].nextGameSchedule.dates[0].games[0].teams.away.team.name
-  const gameDate = new Date().toDateString(data.data.teams[0].nextGameSchedule.dates[0].games[0].gameDate)
+  const gameDate = new Date(data.data.teams[0].nextGameSchedule.dates[0].games[0].gameDate).toDateString()
   
+  console.log(data.data.teams[0].nextGameSchedule.dates[0].games[0].gameDate)
   if (homeTeam === 'Vancouver Canucks'){
       document.querySelector('#next-game').innerHTML =`<p>Next game the canucks play at home against the <span style="background-color: ${backColor}; padding: 0 .5rem">${awayTeam}</span> on <span style="background-color: ${backColor}; padding: 0 .5rem">${gameDate}</span></p>`
 
