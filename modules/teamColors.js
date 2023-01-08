457061ff5e6ae7a23e37c7ec5e38c4b54ef0dd07
Donovan <donovangriffin04@gmail.com>
2023-01-08 08:50:38 -0800

    added a

--- modules/teamColors.js
+++ modules/teamColors.js
@@ -212,7 +212,9 @@ let teams = {
   //teamId is also called which updates the axios request to update selected team name and return correct one
 const teamSelectMenu = document.querySelector('#team-names')
 function teamSelect(e) {
-  e.preventDefault()
+  e.preventDefault()  
+  document.querySelector('#buttons').style.display = 'block'
+  console.log('hel;lo')
   cssVarUpdate()
   getTeamId()
 
