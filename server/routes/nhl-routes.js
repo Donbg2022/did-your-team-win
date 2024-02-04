624f55057d89d772d8e17d2117523f07b67fcbc6
Donbg2022 <106941602+Donbg2022@users.noreply.github.com>
2024-02-04 13:10:53 -0800

    added mongodb connection and moved routes into new folder

---
+++ server/routes/nhl-routes.js
@@ -0,0 +1,36 @@
+import { Express } from "express";
+
+
+const router = Express.Router()
+
+app.get('/nhl-api/:teamAbb/club-schedule', async (req, res) => {
+  const { teamAbb } = req.params;
+  try {
+    const response = await axios.get(`https://api-web.nhle.com/v1/club-schedule-season/${teamAbb}/now`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/nhl-api/:gameID/boxscore', async (req, res) => {
+  const { gameID } = req.params;
+  try {
+    const response = await axios.get(`https://api-web.nhle.com/v1/gamecenter/${gameID}/boxscore`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/nhl-api/standings', async (req, res) => {
+  try {
+    const response = await axios.get("https://api-web.nhle.com/v1/standings/now");
+    res.json(response.data);
+  } catch {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,41 +1,30 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // Add this line
+const cors = require('cors'); 
+const NhlRouter = require('./routes/nhl-routes')
+import mongoose from 'mongoose';
 
+const dbURI = "mongodb+srv://donovangriffin04:<password>@nhl-stats.7r3moui.mongodb.net/?retryWrites=true&w=majority"
+
+connect().catch((e) => {
+  console.log(e)
+})
+
+async function connect() {
+  await mongoose.connect(dbURI)
+  console.log("connected")
+}
+
+//initializes express
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.use(express.json());
-app.use(cors()); // Add this line to enable CORS
 
-app.get('/nhl-api/:teamAbb/club-schedule', async (req, res) => {
-  const { teamAbb } = req.params;
-  try {
-    const response = await axios.get(`https://api-web.nhle.com/v1/club-schedule-season/${teamAbb}/now`);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+app.use(express.json()); //Parse incoming JSON 
+app.use(cors()); //enables CORS
 
-app.get('/nhl-api/:gameID/boxscore', async (req, res) => {
-  const { gameID } = req.params;
-  try {
-    const response = await axios.get(`https://api-web.nhle.com/v1/gamecenter/${gameID}/boxscore`);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+app.use('/nhl-api', NhlRouter)
 
-app.get('/nhl-api/standings', async (req, res) => {
-  try {
-    const response = await axios.get("https://api-web.nhle.com/v1/standings/now");
-    res.json(response.data);
-  } catch {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
