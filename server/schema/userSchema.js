f04b70aa6a4e3ce54f3f7a3a4c093e3b7dd609f2
Donbg2022 <106941602+Donbg2022@users.noreply.github.com>
2024-02-05 11:17:07 -0800

    bcrypt support

--- server/schema/userSchema.js
+++ server/schema/userSchema.js
@@ -20,4 +20,25 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  // Only hash the password if it's modified or a new user
+  if (!user.isModified('password')) return next();
+
+  // Generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+
+    // Hash the password using the generated salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // Override the plaintext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 module.exports = mongoose.model("User", UserSchema)
