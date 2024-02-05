c6458907836c607f7caac71a7662265a8def71b8
Donbg2022 <106941602+Donbg2022@users.noreply.github.com>
2024-02-05 10:12:29 -0800

    changed post URL

--- modules/signUp.js
+++ modules/signUp.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 async function SignUp(newUser){
 console.log("running")
   try{
-    const response = await axios.post("https://whowon.netlify.app/user/signup", newUser)
+    const response = await axios.post("https://nhl-winners.onrender.com/user/signup", newUser)
     console.log(response.data)
   }catch(err){
     console.error(err)
